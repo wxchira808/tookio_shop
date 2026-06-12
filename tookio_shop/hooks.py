c557b76a8295b2d2920f4e4b988201ff74fa353d
wxchira <wacheeemakingbeats@gmail.com>
2026-06-12 23:02:22 +0300

    start 1.01: converting from frappe to tookio-shop-app

--- tookio_shop/hooks.py
+++ tookio_shop/hooks.py
@@ -292,7 +292,7 @@ scheduler_events = {
 # default_log_clearing_doctypes = {
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
-# Serve the Expo web app as a standalone SPA under /app.
+# Serve the Expo web app as a standalone SPA under /tookio_shop.
 website_route_rules = [
-    {"from_route": "/app/<path:app_path>", "to_route": "app"},
+    {"from_route": "/tookio_shop/<path:app_path>", "to_route": "tookio_shop"},
 ]
